5370af001484ab0c97124812cc115131414def79
NAROMBA CONDÉ <naro@MacBook-Air-de-NAROMBA.local>
2025-06-24 14:58:58 -0400

    components

--- PiqueMe/app/firebaseConfig.js
+++ PiqueMe/app/firebaseConfig.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +25,10 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
 
+/*
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+*/
 export default firebaseApp;
