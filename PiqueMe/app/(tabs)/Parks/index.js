bac0a678f2db7a918a03a1a8807a1de29b5fd819
Fahima-Carmen <fahimacarmen@gmail.com>
2025-07-07 20:23:59 -0400

    Page pour details du parc v.1

--- PiqueMe/app/(tabs)/Parks/index.js
+++ PiqueMe/app/(tabs)/Parks/index.js
@@ -10,6 +10,18 @@ const Index = (() => {
                 <Text>Go to Second Page</Text>
             </Link>
 
+            <Text style={{ fontSize: 20, padding: 16 }}>Page des parcs</Text>
+
+
+            <ParcFavoris
+                id="42"
+                name="Parc Jarry"
+                imageUri="https://picsum.photos/700"
+                rating={4.6}
+                reviews={178}
+                distanceKm={3.4}
+            />
+
         </View>
     )
 })
